c4de44a42c119d6fce831af984288d0bffcfe12e
Yue Hu <yhu57782@usc.edu>
2024-07-04 17:43:59 -0700

    just test

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 # python demo.py --imagedir=../splatmap/data/Augmented_ICL-NUIM_Reconstruction_Dataset/livingroom2-color --calib=../splatmap/calib/tum2.txt --reconstruction_path savereconstruction 
 # python demo.py --imagedir=../SplaTAM/data/Replica/room0/rgb --calib=../splatmap/calib/replica.txt --reconstruction_path savereconstruction 
-python demo.py --imagedir=../../../data1/yhu57782/splatmap/data/Replica/room0/rgb --calib=calib/replica.txt --reconstruction_path savereconstruction 
+python demo.py --imagedir=../SplaTAM/data/Replica/room0/rgb --calib=calib/replica.txt --reconstruction_path savereconstruction 
